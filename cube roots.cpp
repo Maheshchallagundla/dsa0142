b86ffce61e72aa52f6b48425b8d64d8b84dc8729
Maheshchallagundla <113338075+Maheshchallagundla@users.noreply.github.com>
2023-03-07 08:46:17 +0530

    Add files via upload

---
+++ cube roots.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+#include<iostream>
+class cube
+{
+public:
+int a, b;
+cube()
+{
+	cout<<" \nEnter the number of finding cube";
+	cin>>a;
+	cout<<" \nThe cube root of number "<<a*a*a;
+	}	
+};
+int main()
+{
+	cout<<"Demonstration for  finding cube root of a number";
+	cube a;
+}
